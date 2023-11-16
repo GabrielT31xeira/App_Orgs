e9f5fd244fd02488749015fcaef2c8bebf202219
Sósthenes Oliveira Lima <sosthenes.lima@gmail.com>
2023-11-16 18:01:54 -0300

    Commit - Up
    Projeto Concluído com sucesso.

---
+++ app/src/main/java/br/com/sosthenes/orgs/dao/ProdutosDao.kt
@@ -0,0 +1,19 @@
+package br.com.sosthenes.orgs.dao
+
+import br.com.sosthenes.orgs.model.Produto
+
+class ProdutosDao {
+
+    fun adiciona(produto: Produto){
+        produtos.add(produto)
+    }
+
+    fun buscaTodos() : List<Produto> {
+        return produtos.toList()
+    }
+
+    companion object {
+        private val produtos = mutableListOf<Produto>()
+    }
+
+}
